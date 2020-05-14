0c4f794f969f607948d5bb5162f3001f2782b6bd
Aditya Patwa <adityapatwa@live.com>
2020-05-14 11:45:59 -0400

    Initial commit.

---
+++ src/main/java/com/learning/springbootrecipeapp/SpringBootRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springbootrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipeAppApplication.class, args);
+    }
+
+}
