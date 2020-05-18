805c5d04e31a99d152c9bc59f5bba49a5d415175
Aditya Patwa <adityapatwa@live.com>
2020-05-18 10:20:49 -0400

    Added a MockMVC test for the Index controller.

--- src/test/java/com/learning/springbootrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/learning/springbootrecipeapp/controllers/IndexControllerTest.java
@@ -5,8 +5,12 @@ import com.learning.springbootrecipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +18,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 
 class IndexControllerTest {
     IndexController indexController;
@@ -24,10 +31,21 @@ class IndexControllerTest {
     @Mock
     Model model;
 
+    @Captor
+    ArgumentCaptor<Set<Recipe>> argumentCaptor;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
@@ -42,8 +60,6 @@ class IndexControllerTest {
         recipes.add(recipe2);
 
         when(recipeService.getRecipes()).thenReturn(recipes);
-
-        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         // When
         String viewName = indexController.getIndexPage(model);
